a36d9489647dd31bb3e670b7df3a22a0378764ba
Lara <laraellenh@gmail.com>
2022-03-06 14:38:01 -0500

    commit tree 1abcfda786f44d24a0ba2c72003a0502571a3fb0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize word
+        # our name symbol is = to word 
+        @name = word
+    end
+
+
+    def match(array)
+        # select filters an array of objects, so that if we use the split method on the string in side the array, and when sorted see if word == name, .sort Returns a new array created by sorting self.
+        array.select{|word|word.split("").sort == name.split("").sort}
+
+    end
+end
